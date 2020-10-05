608c1cf7b81ca37c4b4631e00c4e4d51d7a6bad3
Sarah Julia Gushef <sarahgushef@gmail.com>
2020-10-05 22:18:17 +0700

    Create header

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+import Container from '@material-ui/core/Container';
+
+import Header from 'components/Header';
+
+const StyledContainer = styled(Container)`
+    padding-right: 0 !important;
+    padding-left: 0 !important;
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <StyledContainer maxWidth='sm'>
+            <Header />
+        </StyledContainer>
+    );
 }
 
 export default App;
---
+++ src/components/Header/index.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import DatePicker from 'react-horizontal-datepicker';
+
+const Header = styled.header`
+    position: static;
+    padding: 0 16px;
+    background-color: white;
+    color: black;
+`;
+
+const BackAndAddress = styled.div`
+    display: flex;
+
+    & div:nth-child(1) {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        margin-right: 16px;
+    }
+
+    & p {
+        font-size: 10px;
+        margin-bottom: 0;
+    }
+
+    & span {
+        font-size: '16px';
+        margin-bottom: 0;
+    }
+`;
+
+const StyledExpandMoreIcon = styled(ExpandMoreIcon)`
+    color: red;
+`;
+
+const Calender = styled.div`
+    margin-top: 24px;
+
+    & button,
+    span {
+        display: none;
+    }
+`;
+
+function index() {
+    const selectedDay = (val) => {
+        return val;
+    };
+
+    return (
+        <Header>
+            <BackAndAddress>
+                <div>
+                    <ArrowBackIcon />
+                </div>
+                <div>
+                    <p>ALAMAT PENGIRIMAN</p>
+                    <div>
+                        <span>Tokopedia Tower</span>
+                        <StyledExpandMoreIcon />
+                    </div>
+                </div>
+            </BackAndAddress>
+            <Calender>
+                <DatePicker
+                    getSelectedDay={selectedDay}
+                    endDate={14}
+                    selectDate={new Date()}
+                    labelFormat={'MMMM'}
+                    color={'#424749'}
+                />
+            </Calender>
+        </Header>
+    );
+}
+
+export default index;
